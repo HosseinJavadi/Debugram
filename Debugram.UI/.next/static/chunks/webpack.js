a883876d0f636cc417c82c95d0686f6124a968b7
Hossein <senator.senator.0012@gmail.com>
2022-10-09 18:59:26 +0330

    تغییرات تم

--- Debugram.UI/.next/static/chunks/webpack.js
+++ Debugram.UI/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "724ab9942bfed5d8"; }
+/******/ 		__webpack_require__.h = function() { return "2cf37a989897f893"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Debugram.UI/pages/index.js
+++ Debugram.UI/pages/index.js
@@ -1,3 +1,34 @@
+import Image from "next/image";
+import tailwindConfig from "../tailwind.config";
+import resolveConfig from "tailwindcss/resolveConfig";
+
 export default function Home() {
-  return <h1 className="text-10">حسین</h1>;
+  const theme = require("../public/theme.json");
+
+  return (
+    <>
+      {Object.getOwnPropertyNames(theme).map((n) => {
+        {
+          return (
+            <div
+              className={`w-10% h-10%`}
+              style={{ backgroundColor: n }}
+              onClick={() => {
+                Object.getOwnPropertyNames(theme[n]).map((p) => {
+                  theme[n][p].map((x, index) => {
+                    document.documentElement.style.setProperty(
+                      `--${p}-${index}`,
+                      x
+                    );
+                  });
+                });
+              }}
+            ></div>
+          );
+        }
+      })}
+      <div className="w-100% h-20% bg-back"></div>
+      <div className="w-100% h-20% bg-back-0"></div>
+    </>
+  );
 }
